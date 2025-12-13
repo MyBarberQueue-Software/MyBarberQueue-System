2cacae4726c2cd3259f88b52a27b1e1652590ab8
Joseph <josephdjamba04@gmail.com>
2025-12-13 10:19:26 +0200

    Feat/Added_Swagger_UI

--- MyBarberQueue Web.API/Program.cs
+++ MyBarberQueue Web.API/Program.cs
@@ -1,18 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at aka.ms
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// These lines make Swagger UI accessible without any authentication
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    // Optional: set Swagger UI as the app's root page
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    // c.RoutePrefix = string.Empty; 
+});
 
 app.UseHttpsRedirection();
 
